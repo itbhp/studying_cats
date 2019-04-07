f58486a72184288beea45e71565adb38f194917b
Paolo Sciarra <paolo.sciarra@lastminute.com>
2019-04-07 20:04:19 +0200

    [CH01] tested cats Show type class

---
+++ src/test/scala/sandbox/chapter1/show/ShowTest.scala
@@ -0,0 +1,31 @@
+package sandbox.chapter1.show
+
+import cats.Show
+import org.scalatest.{FunSpec, Matchers}
+import sandbox.models.Cat
+
+class ShowTest extends FunSpec with Matchers {
+
+  describe("Show type class") {
+
+    it("should transform a known type to a string using default instances") {
+      import cats.instances.int._
+      catsStdShowForInt.show(3) shouldEqual("3")
+    }
+
+    it("should transform a known type to a string using default syntax") {
+      import cats.instances.int._
+      import cats.syntax.show._
+      3.show shouldEqual("3")
+    }
+
+    it("should transform a custom type providing an implicit instance") {
+      import cats.syntax.show._
+
+      implicit val catShow: Show[Cat] = Show.show(_.name)
+
+      Cat("fluffy").show shouldEqual "fluffy"
+    }
+  }
+
+}
