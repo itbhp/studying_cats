b6a94888a6b2096322803ba72077a9349dab2f04
Paolo Sciarra <itbhps@gmail.com>
2019-04-28 15:18:09 +0200

    [CH02] implemented add on list of option using monoids

--- src/main/scala/sandbox/chapter2/cats/examples/SuperAdder.scala
+++ src/main/scala/sandbox/chapter2/cats/examples/SuperAdder.scala
@@ -1,18 +1,21 @@
 package sandbox.chapter2.cats.examples
 
 import cats.Monoid
+import cats.instances.option._
+import cats.syntax.semigroup._
 
 object SuperAdder {
 
-  def add(items: List[Int]): Int = {
+  implicit val sumMonoid = new Monoid[Int] {
+    override def empty: Int = 0
 
-    implicit val sumMonoid = new Monoid[Int] {
-      override def empty: Int = 0
+    override def combine(x: Int, y: Int): Int = x + y
+  }
 
-      override def combine(x: Int, y: Int): Int = x + y
-    }
-
+  def add(items: List[Int]): Int =
     items.fold(Monoid[Int].empty)(Monoid[Int].combine)
 
-  }
+
+  def addOption(items: List[Option[Int]]): Option[Int] =
+    items.fold(Monoid[Option[Int]].empty)(_ |+| _)
 }
--- src/test/scala/sandbox/chapter2/cats/examples/SuperAdderTest.scala
+++ src/test/scala/sandbox/chapter2/cats/examples/SuperAdderTest.scala
@@ -4,13 +4,23 @@ import org.scalatest.{FunSpec, Matchers}
 
 class SuperAdderTest extends FunSpec with Matchers {
 
-  describe("Superadder"){
+  describe("Superadder add(items: List[Int])"){
     it("should add empty list to zero"){
       SuperAdder.add(List()) shouldEqual 0
     }
 
     it("should add non empty list"){
       SuperAdder.add(List(1,2,3,6)) shouldEqual 12
+    }
+  }
+
+  describe("Superadder addOption(items: List[Int])"){
+    it("should add empty list to zero"){
+      SuperAdder.addOption(List()) shouldEqual None
+    }
+
+    it("should add non empty list with"){
+      SuperAdder.addOption(List(Some(1),None,Some(3),Some(6))) shouldEqual Some(10)
     }
   }
 
