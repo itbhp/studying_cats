db5c7478f7fe8a052a9641f7ebc743bcbeead3be
Paolo Sciarra <itbhps@gmail.com>
2019-05-06 21:16:41 +0200

    [CH03] minor refactoring: using andThen

--- src/main/scala/sandbox/chapter3/PrintableFunctors.scala
+++ src/main/scala/sandbox/chapter3/PrintableFunctors.scala
@@ -6,7 +6,8 @@ import sandbox.chapter1.printable.Printable
 object PrintableFunctors {
 
   implicit val contravariantFunctor = new Contravariant[Printable] {
-    override def contramap[A, B](fa: Printable[A])(f: B => A): Printable[B] = (b: B) => fa.format(f(b))
+    override def contramap[A, B](fa: Printable[A])(f: B => A): Printable[B] =
+      (b: B) => ( f andThen fa.format)(b)
   }
 
 }
