038aa021aecce319a563b17c35c5d710fffeec21
Paolo Sciarra <itbhps@gmail.com>
2019-05-06 22:06:38 +0200

    [CH03] instance of Codec[Box[A]]

--- src/test/scala/sandbox/chapter3/CodecTest.scala
+++ src/test/scala/sandbox/chapter3/CodecTest.scala
@@ -16,4 +16,14 @@ class CodecTest extends FunSuite with Matchers {
   test("encode") {
     encode[Double](123.4) shouldEqual "123.4"
   }
+
+  test("with Box") {
+    case class Box[A](a: A)
+
+    implicit def boxCodec[A](implicit codec: Codec[A]): Codec[Box[A]] = codec.imap(a => Box(a), box => box.a)
+
+    decode[Box[Double]]("123.4") shouldEqual Box(123.4)
+    encode[Box[Double]](Box(123.4)) shouldEqual "123.4"
+
+  }
 }
