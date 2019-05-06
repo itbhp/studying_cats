34dd75c3eb56751d67ab965a4f807d6a03ae61fa
Paolo Sciarra <itbhps@gmail.com>
2019-05-06 21:28:47 +0200

    [CH03] defined printable instance for Box[A] using contramap and an implicit val for Printable[A]

--- src/main/scala/sandbox/chapter1/printable/PrintableInstances.scala
+++ src/main/scala/sandbox/chapter1/printable/PrintableInstances.scala
@@ -3,5 +3,6 @@ package sandbox.chapter1.printable
 object PrintableInstances {
 
   implicit val printableInt: Printable[Int] = _.toString
+  implicit val printableString: Printable[String] = (a: String) => a
 
 }
--- src/test/scala/sandbox/chapter3/ContravariantFunctorTest.scala
+++ src/test/scala/sandbox/chapter3/ContravariantFunctorTest.scala
@@ -17,4 +17,19 @@ class ContravariantFunctorTest extends FunSuite with Matchers {
 
     Person("Paolo", 34).format() shouldEqual "34"
   }
+
+  test("contramap with boxes ") {
+    final case class Box[A](value: A)
+
+    import PrintableFunctors.contravariantFunctor
+    import cats.syntax.contravariant._
+    import sandbox.chapter1.printable.PrintableInstances._
+    import sandbox.chapter1.printable.PrintableSyntax._
+
+    implicit def boxPrintableInstance[A](implicit printable: Printable[A]): Printable[Box[A]] =
+      printable.contramap(_.value)
+
+    Box("hello world").format() shouldEqual "hello world"
+    Box(56).format() shouldEqual "56"
+  }
 }
