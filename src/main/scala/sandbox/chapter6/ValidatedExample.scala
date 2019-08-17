aa40469139766cd81673add6c975864e77025ba1
Paolo Sciarra <itbhps@gmail.com>
2019-08-17 17:54:14 +0200

    [ch06] refactor validated example: extracted getValue from FormData

--- src/main/scala/sandbox/chapter6/ValidatedExample.scala
+++ src/main/scala/sandbox/chapter6/ValidatedExample.scala
@@ -5,8 +5,6 @@ import cats.syntax.either._
 import cats.syntax.apply._
 import cats.instances.list._
 
-import scala.util.Try
-
 object ValidatedExample {
 
   case class User(name: String, age: Int)
@@ -21,14 +19,17 @@ object ValidatedExample {
     (readName(formData).toValidated, readAge(formData).toValidated).mapN(User.apply)
 
   def readName(formData: FormData): FailFastOr[String] =
-    formData.get("name")
-      .toRight(List("Name not found"))
+    getValue(formData, "name")
       .ensure(List("Name empty"))(!_.isEmpty)
 
   def readAge(formData: FormData): FailFastOr[Int] =
-    formData.get("age")
-      .toRight(List("Age not found"))
+    getValue(formData, "age")
       .ensure(List("Age empty"))(!_.isEmpty)
       .flatMap(age => Either.catchOnly[NumberFormatException](age.toInt).leftMap(_ => List("Age not an int")))
+
+  private def getValue(formData: FormData, fieldName: String): Either[List[String], String] = {
+    formData.get(fieldName)
+      .toRight(List(s"${fieldName.capitalize} not found"))
+  }
 
 }
