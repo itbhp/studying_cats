50c5c52b3b48acd00865d6c6be8221e975cc8d0a
Paolo Sciarra <itbhps@gmail.com>
2019-08-17 18:06:50 +0200

    [ch06] refactor validated example: used for comprehension

--- src/main/scala/sandbox/chapter6/ValidatedExample.scala
+++ src/main/scala/sandbox/chapter6/ValidatedExample.scala
@@ -18,15 +18,20 @@ object ValidatedExample {
   def readUser(formData: FormData): FailAtTheEndOr[User] =
     (readName(formData).toValidated, readAge(formData).toValidated).mapN(User.apply)
 
-  def readName(formData: FormData): FailFastOr[String] = {
-    getValue(formData, "name").flatMap(v => nonEmpty(v, "name"))
-  }
+  def readName(formData: FormData): FailFastOr[String] =
+    for{
+      name <- getValue(formData, "name")
+      _    <- nonEmpty(name, "name")
+    } yield name
 
-  def readAge(formData: FormData): FailFastOr[Int] = {
-    getValue(formData, "age")
-      .flatMap(v => nonEmpty(v, "age"))
-      .flatMap(age => parseInt(age, "Age not an int"))
-  }
+
+  def readAge(formData: FormData): FailFastOr[Int] =
+    for {
+      ageStr <- getValue(formData, "age")
+      _      <- nonEmpty(ageStr, "age")
+      age    <- parseInt(ageStr, "Age not an int")
+    } yield age
+
 
   private def parseInt(age: String, errorMessage: String): Either[List[String], Int] = {
     Either.catchOnly[NumberFormatException](age.toInt).leftMap(_ => List(errorMessage))
